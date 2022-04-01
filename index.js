53f3b7f932f766cad6c226cc4cef423e53cdd079
JMG24a <jmg24a@gmail.com>
2022-04-01 10:51:59 -0400

    configuracion de ruta docs con swagger

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const appRoutes = require('./network')
+const appDocs = require('./docs')
+const errors = require('./middleware/error')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+appRoutes(app)
+appDocs(app)
+
+app.use(errors)
+
+app.listen(3001, ()=>{
+    console.log('[SERVER] on port 3001 is run')
+})
